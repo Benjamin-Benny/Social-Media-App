ec61fa514be471baaa15bb3f8606bc949e440db0
Benjamin Benny <35957758+Benjamin-Benny@users.noreply.github.com>
2024-06-26 12:57:58 +0530

    added service

--- app/src/app/post-list/post-list.component.ts
+++ app/src/app/post-list/post-list.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
+  posts: any[] = [];
+  users: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+    this.fetchUsers();
+  }
+
+  fetchPosts() {
+    this.apiService.getPosts().subscribe((posts: any[]) => {
+      this.posts = posts;
+      this.posts.forEach(post => {
+        post.userName = '';
+      });
+      this.assignUserNames();
+    });
+  }
+
+  fetchUsers() {
+    this.apiService.getUsers().subscribe((users: any[]) => {
+      this.users = users;
+      this.assignUserNames();
+    });
+  }
+
+  assignUserNames() {
+    if (this.posts.length && this.users.length) {
+      this.posts.forEach(post => {
+        const user = this.users.find(u => u.id === post.userId);
+        if (user) {
+          post.userName = user.name;
+        }
+      });
+    }
+  }
 }
